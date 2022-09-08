8248e3883607ce0129e269b7eec07dbf9ee73b55
Nimod <d1mitry.sidorov@yandex.ru>
2022-09-08 11:36:15 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Module14/05_least_divisor/main.py
    #	Module14/06_coin2/main.py

--- Module15/09_word_analysis_2/main.py
+++ Module15/09_word_analysis_2/main.py
@@ -1,14 +1,14 @@
 word = input('Введите слово: ')
 word_list = list(word)
 counter = 0
-palindrom = 1
+palindrom = True
 for i in range(round(len(word_list) / 2)):
     if word_list[i] == word_list[-1-counter]:
         counter += 1
     else:
-        palindrom = 0
+        palindrom = False
         break
-if palindrom == 1 :
+if palindrom == True :
     print('Это Палиндром!')
 else:
     print('Это Не Палиндром!')
