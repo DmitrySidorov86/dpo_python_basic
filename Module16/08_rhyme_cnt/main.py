62a1fa166595573fe708f06c82c3a48b57e6ade1
Nimod <d1mitry.sidorov@yandex.ru>
2022-10-05 16:14:58 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Module14/05_least_divisor/main.py
    #	Module14/06_coin2/main.py

--- Module16/08_rhyme_cnt/main.py
+++ Module16/08_rhyme_cnt/main.py
@@ -21,33 +21,18 @@ man_list = list(range(1, number + 1))
 begin_number = 0
 counter = counting
 
-if len(man_list) == 1:
-    print(f'\nОстался человек под номером {man_list[0]}')
-else:
-    while True:
+while len(man_list) > 1:
+    print(f'\nТекущий круг людей: {man_list}')
+    print(f'Начало счёта с номера:{man_list[begin_number]} ')
+    if counter >= len(man_list[begin_number:]):
+        counter -= len(man_list[begin_number:])
+        counter %= len(man_list)
+        delete(counter - 1)
+        begin_number = census(man_list)
+        counter = counting
 
-        if man_list[begin_number] in man_list:
-            print(f'\nТекущий круг людей: {man_list}')
-            print(f'Начало счёта с номера:{man_list[begin_number]} ')
-            if counter >= len(man_list[begin_number:]):
-                counter -= len(man_list[begin_number:])
-                counter %= len(man_list)
-                delete(counter - 1)
-                if len(man_list) == 1:
-                    print(f'\nОстался человек под номером {man_list[0]}')
-                    break
-                else:
-                    begin_number = census(man_list)
-                    counter = counting
-
-            elif counter < len(man_list[begin_number:]):
-                delete(begin_number + counter - 1)
-                if len(man_list) == 1:
-                    print(f'\nОстался человек под номером {man_list[0]}')
-                    break
-                begin_number = census(man_list)
-                counter = counting
-        else:
-            print(f'Игрока с номером {begin_number} нет среди участников.\n'
-                  f'Попробуйте еще раз!')
-            begin_number = census(man_list)
+    elif counter < len(man_list[begin_number:]):
+        delete(begin_number + counter - 1)
+        begin_number = census(man_list)
+        counter = counting
+print(f'\nОстался человек под номером {man_list[0]}')
